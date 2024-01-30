bd15d51154788a051882f20ca97efb418d994654
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-01-30 16:30:17 +0900

    Initial Commit

---
+++ Media/Viewcontroller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media
+//
+//  Created by hwijinjeong on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
